63e46a01279c0136c71581f5fd57e8b16a3b0945
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-16 14:14:51 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Ihor Sukhachov on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
