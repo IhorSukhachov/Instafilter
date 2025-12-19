e3fff6cce07847b6e8638e043f2f750dea760dad
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-19 15:21:10 +0200

    making basic ui

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 
 struct ContentView: View {
-   
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-       
-}
+        VStack {
+            Spacer()
+            
+            //image  area
+            
+            Spacer()
+            
+            HStack {
+                Text("Intensity")
+                Slider(value: $filterIntensity)
+            }
+            
+            HStack {
+                Button("Change filter") {
+                    //some code later
+                }
+                
+                Spacer()
+                
+            }
+        }.padding([.horizontal, .bottom])
+        
+        
+    }
+    
 }
 
 #Preview {
