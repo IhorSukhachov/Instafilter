b31ed8a6f12a991a4debcbdd5cc7f7d87bab00e4
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-19 16:15:27 +0200

    applying filter to the image

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -62,6 +62,10 @@ struct ContentView: View {
         Task {
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
             guard let inputImage = UIImage(data: imageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
         }
     }
     
